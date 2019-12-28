c23f40ff438586e1cfc988dd4413b9b4f5d5a65c
PRINCEISAH <37813957+PRINCEISAH@users.noreply.github.com>
2019-12-28 18:13:49 -0500

    Add files via upload

---
+++ multiplication table.kt
@@ -0,0 +1,19 @@
+
+fun main(args:Array<String>) {
+    
+Multiplcation_Table()
+}
+fun Multiplcation_Table(){
+
+
+    var leng = 1..12
+
+
+
+
+    for (x in leng)
+
+
+        println("$x * 2 = "+ x *2 +"  "+ "$x * 3 = " +x*3 +"  "+ "$x * 4 = "+x*4  )
+
+}
